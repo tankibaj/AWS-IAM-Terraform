a89832744e13b43ae92cd3ef9a216421d1ddbbc1
Naim <mail@thenaim.com>
2021-04-24 22:33:04 +0200

    group init

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
-output "this_iam_user_name" {
-  description = "The user's name"
-  value       = module.iam_user.this_iam_user_name
-}
-
-output "this_iam_user_login_profile_encrypted_password" {
-  description = "The encrypted password, base64 encoded"
-  value       = module.iam_user.this_iam_user_login_profile_encrypted_password
-}
-
-output "this_iam_access_key_id" {
-  description = "The access key ID"
-  value       = module.iam_user.this_iam_access_key_id
-}
-
-output "this_iam_access_key_encrypted_secret" {
+output "adminuser_access_key_secret" {
   description = "The encrypted secret, base64 encoded"
-  value       = module.iam_user.this_iam_access_key_encrypted_secret
+  value       = module.adminuser.this_iam_access_key_encrypted_secret
+}
+
+output "adminuser_access_key_id" {
+  description = "The access key ID"
+  value       = module.adminuser.this_iam_access_key_id
+}
+
+output "adminuser_user_login_profile_password" {
+  description = "The encrypted password, base64 encoded"
+  value       = module.adminuser.this_iam_user_login_profile_encrypted_password
+}
+
+output "readonlyuser_access_key_secret" {
+  description = "The encrypted secret, base64 encoded"
+  value       = module.readonlyuser.this_iam_access_key_encrypted_secret
+}
+
+output "readonlyuser_access_key_id" {
+  description = "The access key ID"
+  value       = module.readonlyuser.this_iam_access_key_id
 }
